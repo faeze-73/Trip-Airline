a74138ab88aa451d49e16fd86545ddfcd381a2f6
faeze <faeze.fallahnezhad73@gmail.com>
2025-05-10 22:18:32 +0330

    feat: add exChange & searchNormal & airPlane icon component

--- app/components/base/Icon.tsx
+++ app/components/base/Icon.tsx
@@ -1,19 +1,24 @@
 import IconUser from "../icons/IconUser";
 import IconPhone from "../icons/IconPhone"
+import AirPlane from "../icons/AirPlane";
+import ExChange from "../icons/ExChange";
 
 const iconsMap = {
   user: IconUser,
   phone: IconPhone,
+  airPlane: AirPlane,
+  exChange: ExChange
 };
 
 type IconProps = {
   name: keyof typeof iconsMap;
   size?: number;
+  color?: string;
   className?: string;
 };
 
-export default function Icon({ name, size = 24, className = "" }: IconProps) {
+export default function Icon({ name, size = 24, color, className = "" }: IconProps) {
   const IconComponent = iconsMap[name];
   if (!IconComponent) return null;
-  return <IconComponent width={size} height={size} className={className} />;
+  return <IconComponent width={size} height={size} className={className} color={color} />;
 }
