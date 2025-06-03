0fcba65114927413fb779cecc0ff348492e0592f
faeze <faeze.fallahnezhad73@gmail.com>
2025-06-03 15:59:49 +0330

    feat: improve BaseButton component with new features

--- app/components/base/BaseButton.tsx
+++ app/components/base/BaseButton.tsx
@@ -7,6 +7,7 @@ type BaseButtonProps = {
   className?: string;
   onClick?: () => void;
   textColor?: string;
+  colSpan?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 };
 
 export default function BaseButton({
@@ -16,10 +17,13 @@ export default function BaseButton({
   className = "",
   onClick,
   textColor = "text-white",
+  colSpan = 12,
 }: BaseButtonProps) {
+  const widthClass = `w-${colSpan}/12`;
+
   return (
     <button
-      className={`bg-primary hover:bg-primary-shade-1 px-4 py-2 rounded-lg flex items-center justify-center gap-2 ${textColor} ${className}`}
+      className={`bg-primary hover:bg-primary-shade-1 px-4 py-2 rounded-lg flex items-center justify-center gap-2 ${textColor} ${widthClass} ${className}`}
       onClick={onClick}
     >
       {iconLeft && <span className="flex items-center">{iconLeft}</span>}
