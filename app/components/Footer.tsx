8a60a29e7b77c1800591e92e620357154adfdc40
faeze <faeze.fallahnezhad73@gmail.com>
2025-12-07 15:39:29 +0330

    refactor(footer): replace img with next/image Image component

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import Icon from "./base/Icon";
+import Image from "next/image";
 
 const usefulLinks = [
   { label: "About Us", href: "#" },
@@ -25,7 +26,7 @@ const Footer: React.FC = () => (
         </div>
         <div className="flex gap-4">
           <a href="#" className="inline-block">
-            <img
+            <Image
               src="/appleStore.png"
               alt="Apple Store"
               width={182}
@@ -34,7 +35,7 @@ const Footer: React.FC = () => (
             />
           </a>
           <a href="#" className="inline-block">
-            <img
+            <Image
               src="/playStore.png"
               alt="Play Store"
               width={182}
@@ -65,7 +66,7 @@ const Footer: React.FC = () => (
         </div>
         {/* Address & Support */}
         <div className="flex-1 flex flex-col items-center md:items-start">
-          <img src="/logo.svg" alt="Bilito Logo" width={100} className="mb-2" />
+          <Image src="/logo.svg" alt="Bilito Logo" width={100} className="mb-2" />
           <div className="font-bold text-primary text-lg mb-1">Bilito</div>
           <div className="mb-1">
             Support:{" "}
